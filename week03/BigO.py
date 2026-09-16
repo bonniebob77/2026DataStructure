68a1fa92ec5d607f317e351452f535f3aba14dc6
bonnie <bonniebob77@gmail.com>
2026-09-16 12:14:04 +0900

    week03 v0.5 O(1)

--- week03/BigO.py
+++ week03/BigO.py
@@ -1,7 +1,5 @@
-n = int(input("정수 입력 :"))
-total = 0
-for i in range(1, n+1):
-    total = total + i
+n = int(input("정수 입력 : "))
+total = n * (n+1) //2
 print(f"1 부터 {n}까지 누산 합계는 {total}입니다.")
-# f(n) = n + 3
-# O(n) 선형시간
+# f(n) = 3 = 1 + 2 = 1 (결국 2번줄로 끝)
+# O(1) 상수시간
