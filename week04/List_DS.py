62f3e1f5db428d41a5c93027904ed209f6c60d71
bonnie <bonniebob77@gmail.com>
2026-09-23 11:18:04 +0900

    week04 v0.8

---
+++ week04/List_DS.py
@@ -0,0 +1,10 @@
+# artists = []
+artists = list()
+print(artists)
+artists.append("리센느") #0번방
+print(artists)
+artists.append("핑클") #1번방
+artists.append("데이식스") #2번방
+print(artists)
+print(artists.pop(1)) #1번방 삭제
+print(artists)
